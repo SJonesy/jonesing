2572c1106fd670a6a7484c1df97164685a173c35
Stephen Jones <stephen.jones@whiteboard-it.com>
2014-06-30 22:32:13 -0500

    add section 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
     verticalCentered: true,
     resize : true,
-    sectionsColor : ['#ccc', '#fff'],
-    anchors:['firstSlide', 'secondSlide'],
+    sectionsColor : ['#2e2e2e', '#53868B', '#2e2e2e', '#e2e2e2'],
+    anchors:['firstSlide', 'secondSlide', 'thirdSlide', 'fourthSlide'],
     scrollingSpeed: 700,
     easing: 'easeInQuart',
     menu: false,
@@ -18,7 +18,7 @@ $(document).ready(function() {
     autoScrolling: true,
     scrollOverflow: false,
     css3: false,
-    paddingTop: '3em',
+    paddingTop: '0px',
     paddingBottom: '10px',
     normalScrollElements: '#element1, .element2',
     normalScrollElementTouchThreshold: 5,
@@ -29,10 +29,21 @@ $(document).ready(function() {
 
     //events
     onLeave: function(index, nextIndex, direction){},
-    afterLoad: function(anchorLink, index){},
-    afterRender: function(){},
+    afterLoad: function (anchorLink, index) {
+      if (anchorLink === 'secondSlide') {
+
+      }
+    },
+    afterRender: function(){
+      setTimeout(function(){$('.section.hi .say_hi').css('opacity', 1);}, 1000);
+      setTimeout(function(){$('.section.hi .my_name').css('opacity', 1);}, 2000);
+      setTimeout(function(){$('.section.hi .down_arrow').css('opacity', 1);}, 5000);
+    },
     afterResize: function(){},
-    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-    onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+    },
+    onSlideLeave: function(anchorLink, index, slideIndex, direction){
+      console.log("SLIDE LEAVE");
+    }
   });
 });
